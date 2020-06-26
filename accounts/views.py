04502d1ecc5660da72a9e127737a477b1a2eb42f
thirunayan <thirunayan22@gmail.com>
2020-06-26 14:52:36 +0530

    commit-2_template inheritance

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render #render will render an html page
 from django.http import HttpResponse
 
 #This is where the views should be created
 
 # Create your views here.
 def home(request):
-    return HttpResponse('home')
+    return render(request,'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('products')
+    return render(request,'accounts/products.html')
 
 def customer(request):
-    return HttpResponse('customer')
+    return render(request,'accounts/customer.html')
+    # return HttpResponse('customer')
+
+
 
 
  
